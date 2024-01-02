cffe4f6e5645f3d756f9ebd8399333eb7ef24c48
Hetav21 <shahhetav2106@gmail.com>
2024-01-02 18:44:39 +0530

    Completing medium questions

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -3,8 +3,22 @@
   Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
 */
 
+
 function isPalindrome(str) {
+
+  str = str.toUpperCase().replaceAll("!", "").replaceAll(",","").replaceAll("?","").replaceAll(".","").replaceAll(" ","");
+
+  console.log(str);
+
+  for(var i = 0; i < str.length / 2; i++){
+    var j = str.length - i - 1;
+
+    if(str[i] !== str[j]) return false;
+  }
+ 
   return true;
 }
+
+// console.log(isPalindrome("Able, was I ere I saw Elba!"));
 
 module.exports = isPalindrome;
