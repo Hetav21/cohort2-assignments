7d8bc61f9c0bf9a19bb0884e362309b809511b13
Hetav21 <shahhetav2106@gmail.com>
2024-01-17 14:45:52 +0530

    Completing Week 3.3 Mongo Assignment Part 2

--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -1,22 +1,73 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { User, Course } = require("../db");
 
 // User Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement user signup logic
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const newUser = await User.create({
+        username,
+        password,
+    })
+
+    res.status(200).json({
+        msg: "User Created Succesfully",
+        newUser
+    });
 });
 
-router.get('/courses', (req, res) => {
+router.get('/courses', async (req, res) => {
     // Implement listing all courses logic
+    
+    const resposne = await Course.find({});
+
+    res.status(200).json({
+        Courses: resposne
+    });
+
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
+router.post('/courses/:courseId', userMiddleware, async (req, res) => {
     // Implement course purchase logic
+
+    const courseId = req.params.courseId;
+    const username = req.headers.username;
+
+    const newCourse = await User.updateOne({
+        username: username
+    }, {
+        "$push": {
+            purchasedCourses: courseId
+        }
+    })
+
+    res.status(200).json({
+        msg: "Course Added Succesfully",
+    });
+
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
+router.get('/purchasedCourses', userMiddleware, async (req, res) => {
     // Implement fetching purchased courses logic
+    const user = await User.findOne({
+        username: req.headers.username
+    })
+
+    const courses = await Course.find({
+        _id: {
+            "$in": user.purchasedCourses
+        }
+    })
+
+    res.status(200).json({
+        courses: courses
+    })
+
 });
 
 module.exports = router
